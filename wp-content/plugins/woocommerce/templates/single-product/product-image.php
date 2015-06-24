7afd44f3fe06662e6738e159f90e2781e5a8eb71
Pablo Damico <pablo.damico88@gmail.com>
2015-06-23 22:13:15 -0300

    ss

--- wp-content/plugins/woocommerce/templates/single-product/product-image.php
+++ wp-content/plugins/woocommerce/templates/single-product/product-image.php
@@ -12,6 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $post, $woocommerce, $product;
 ?>
 <div class="images">
+	<?php echo $image_link;?>
+	<?php echo get_template_directory(); ?>
 	<img class="included" src="<?php echo ABSPATH; ?>wp-content/uploads/arte-shipping-included-2.png">
 	<?php
 		if ( has_post_thumbnail() ) {
